c7fe1a9d78216476d1eee85a64230608a8480045
Oleksandr Kushchenko <gearok@gmail.com>
2016-05-27 02:53:39 +0300

    Add templates, templating logic, scripts

---
+++ build_and_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+set -e
+
+cur_dir="$PWD"
+geant_version="10.2.1"
+lab_name="B1"
+geant_dir="/Users/oleksa/univ/Geant4-${geant_version}-Darwin"
+
+rm -r "${cur_dir}/${lab_name}-build"
+mkdir "${cur_dir}/${lab_name}-build"
+cd "${cur_dir}/${lab_name}-build"
+cmake -DGeant4_DIR="${geant_dir}/lib/Geant4-${geant_version}" "../${lab_name}"
+make -j1
+make install
+
+cd "${geant_dir}/share/Geant4-${geant_version}/geant4make"
+source ./geant4make.sh
+cd "${cur_dir}/${lab_name}-build"
+/usr/local/bin/exampleB1
+cd ${cur_dir}
---
+++ lab.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from jinja2 import Environment, FileSystemLoader
+import subprocess
+import os
+
+lab_name = "B1"
+
+params_detector = {}
+params_detector["environment_material"] = os.getenv("environment_material", "AIR")
+params_detector["world_material"] = os.getenv("world_material", "AIR")
+params_detector["array_material"] = os.getenv("array_material", "Pb")
+params_detector["detector_material"] = os.getenv("detector_material", "Pb")
+params_detector["detector_size"] = os.getenv("detector_size", 2.5) # in cm
+
+params_generator = {}
+params_generator["particle_energy"] = os.getenv("particle_energy", 6.0) # in MeV
+
+files = {
+            "B1DetectorConstruction.cc": params_detector,
+            "B1PrimaryGeneratorAction.cc": params_generator
+        }
+
+for name, args in files.iteritems():
+    env = Environment(loader=FileSystemLoader('{0}/src'.format(lab_name)))
+    template = env.get_template(name + ".j2")
+    output_from_parsed_template = template.render(**args)
+    print output_from_parsed_template
+    # to save the results
+    with open(name, "wb") as fh:
+        fh.write(output_from_parsed_template)
+
+subprocess.call("./build_and_run.sh")
