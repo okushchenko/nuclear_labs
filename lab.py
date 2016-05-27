2e3952c9cbeba312be614c101f4a49896138a55f
Oleksandr Kushchenko <gearok@gmail.com>
2016-05-27 03:06:06 +0300

    Template files to the correct directory

--- lab.py
+++ lab.py
@@ -10,10 +10,10 @@ params_detector["environment_material"] = os.getenv("environment_material", "AIR
 params_detector["world_material"] = os.getenv("world_material", "AIR")
 params_detector["array_material"] = os.getenv("array_material", "Pb")
 params_detector["detector_material"] = os.getenv("detector_material", "Pb")
-params_detector["detector_size"] = os.getenv("detector_size", 2.5) # in cm
+params_detector["detector_size"] = float(os.getenv("detector_size", 2.5)) # in cm
 
 params_generator = {}
-params_generator["particle_energy"] = os.getenv("particle_energy", 6.0) # in MeV
+params_generator["particle_energy"] = float(os.getenv("particle_energy", 6.0)) # in MeV
 
 files = {
             "B1DetectorConstruction.cc": params_detector,
@@ -26,7 +26,7 @@ for name, args in files.iteritems():
     output_from_parsed_template = template.render(**args)
     print output_from_parsed_template
     # to save the results
-    with open(name, "wb") as fh:
+    with open('{0}/src/'.format(lab_name) + name, "wb") as fh:
         fh.write(output_from_parsed_template)
 
 subprocess.call("./build_and_run.sh")
