461d56301fe82026ad5c36db5cfbf43dc4e80a1f
Oleksandr Kushchenko <gearok@gmail.com>
2016-06-06 11:57:50 +0300

    Check if dir exists before removing. Add README

---
+++ README.txt
@@ -0,0 +1,7 @@
+https://github.com/okushchenko/nuclear_labs
+
+# Посмотреть параметры
+cat ./lab.py
+
+# Запустить симуляцию
+env particle_energy="1250" detector_material="Al" ./lab.py
--- build_and_run.sh
+++ build_and_run.sh
@@ -7,7 +7,9 @@ geant_version="10.2.1"
 lab_name="B1"
 geant_dir="/Users/oleksa/univ/Geant4-${geant_version}-Darwin"
 
-rm -r "${cur_dir}/${lab_name}-build"
+if [ -d "${cur_dir}/${lab_name}-build" ]; then
+    rm -r "${cur_dir}/${lab_name}-build"
+fi
 mkdir "${cur_dir}/${lab_name}-build"
 cd "${cur_dir}/${lab_name}-build"
 cmake -DGeant4_DIR="${geant_dir}/lib/Geant4-${geant_version}" "../${lab_name}"
